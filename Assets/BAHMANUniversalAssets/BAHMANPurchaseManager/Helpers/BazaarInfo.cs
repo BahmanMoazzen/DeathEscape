ade5694887d9c481635012c00af573ffea5d7f52
Bahman Moazzen <bahmanmoazzen@gmail.com>
2025-01-12 10:55:00 +0330

    Pass 1 with debug text

--- Assets/BAHMANUniversalAssets/BAHMANPurchaseManager/Helpers/BazaarInfo.cs
+++ Assets/BAHMANUniversalAssets/BAHMANPurchaseManager/Helpers/BazaarInfo.cs
@@ -1,4 +1,6 @@
+using BazaarInAppBilling;
 using UnityEngine;
+using UnityEngine.Events;
 [CreateAssetMenu(fileName = "NewBazaar", menuName = "Bahman/Bazaar", order = 1)]
 public class BazaarInfo : MarketInfo
 {
@@ -6,15 +8,31 @@ public class BazaarInfo : MarketInfo
     {
         throw new System.NotImplementedException();
     }
+    public override void _BuyWholeProcess(string iSKU, UnityAction iOnSuccess, UnityAction iOnFail)
+    {
+        base._BuyWholeProcess(iSKU, iOnSuccess, iOnFail);
+        StoreHandler.instance.Purchase(_SKUtoIndex(iSKU), onBuyFailed, onBuySuccess);
 
+    }
     public override void _DisposeMarket()
     {
-        throw new System.NotImplementedException();
-    }
 
+    }
+    protected int _SKUtoIndex(string iSKU)
+    {
+        for (int i = 0; i < _SKUs.Length; i++)
+        {
+            if (_SKUs[i] == iSKU)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
     public override void _InitializeMarket()
     {
-        throw new System.NotImplementedException();
+        _dLog("initializing ...");
+        StoreHandler.instance.InitializeBillingService(onInitFailed, onInitSuccess);
     }
 
     public override void _PurchaseProduct(string iSKU)
@@ -29,11 +47,55 @@ public class BazaarInfo : MarketInfo
 
     public override void _QueryPurchases()
     {
-        throw new System.NotImplementedException();
+        foreach (string s in _SKUs)
+        {
+            StoreHandler.instance.CheckInventory(_SKUtoIndex(s), null, onQueryInventorySuccess);
+        }
+
     }
 
     public override void _QuerySKUDetails()
     {
         throw new System.NotImplementedException();
     }
+
+
+
+    void onInitFailed(int iErrorNumber, string iErrorMessage)
+    {
+        _dLog("Init Failed: " + iErrorMessage);
+    }
+    void onInitSuccess()
+    {
+        _dLog("Init Success. Querying ...");
+        _QueryPurchases();
+    }
+    void onQueryInventorySuccess(Purchase iPurchase, int iIndex)
+    {
+        _dLog(iPurchase.productId + " is in inventory. Consuming ...");
+        StoreHandler.instance.ConsumePurchase(iPurchase, iIndex, null, onConsumeSuccess);
+    }
+    void onBuyFailed(int iErrorNumber, string iErrorMessage)
+    {
+        _dLog("Buy failed");
+        _onFail();
+    }
+    void onBuySuccess(Purchase iPurchase, int iIndex)
+    {
+        _dLog("Buy Success" + iPurchase.purchaseToken);
+        StoreHandler.instance.ConsumePurchase(iPurchase, iIndex, onConsumeFailed, onConsumeSuccess);
+    }
+
+    void onConsumeFailed(int iErrorNumber, string iErrorMessage)
+    {
+        _dLog("Consume failed");
+        _onFail();
+    }
+
+    void onConsumeSuccess(Purchase iPurchase, int iIndex)
+    {
+        _dLog("Consume Success" + iPurchase.purchaseToken);
+        _onSuccess();
+    }
+
 }
